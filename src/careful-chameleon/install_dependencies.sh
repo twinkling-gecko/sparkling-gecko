bc0fee454937ed7e314bf1de4ba3633bd0d4cb1c
Ryota Kobayashi <38117745+kobar9568@users.noreply.github.com>
2020-11-16 13:22:38 +0900

    [CC] fix: espressif32 library resolv issue

--- src/careful-chameleon/install_dependencies.sh
+++ src/careful-chameleon/install_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 docker-compose run -u $(id -u $USER):$(id -g $USER) platformio project init --ide vim --board m5stack-core-esp32
+docker-compose run -u $(id -u $USER):$(id -g $USER) platformio platform install espressif32
 
 sed -i -e 's/\/tmp\/platformio\//.\//' -e "s/\/home\/$USER\/.platformio\//.\//" .ccls
