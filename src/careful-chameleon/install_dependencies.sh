d9266bc319230045764f8ea7a2cd74d21e4679cf
Ryota Kobayashi <38117745+kobar9568@users.noreply.github.com>
2020-11-16 13:52:57 +0900

    [CC] feat: add HX711 library manual install

--- src/careful-chameleon/install_dependencies.sh
+++ src/careful-chameleon/install_dependencies.sh
@@ -4,3 +4,11 @@ docker-compose run -u $(id -u $USER):$(id -g $USER) platformio project init --id
 docker-compose run -u $(id -u $USER):$(id -g $USER) platformio platform install espressif32
 
 sed -i -e 's/\/tmp\/platformio\//.\//' -e "s/\/home\/$USER\/.platformio\//.\//" .ccls
+
+# Private library
+
+## HX711
+mkdir ./lib/HX711/
+wget -P ./lib/HX711/ https://raw.githubusercontent.com/bogde/HX711/master/src/HX711.cpp
+wget -P ./lib/HX711/ https://raw.githubusercontent.com/bogde/HX711/master/src/HX711.h
+echo "-I./lib/HX711" >> .ccls
