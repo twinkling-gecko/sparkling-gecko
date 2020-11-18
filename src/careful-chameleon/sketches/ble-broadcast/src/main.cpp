3cedf368338fbc327198028ef58649924925a8d2
Ryota Kobayashi <38117745+kobar9568@users.noreply.github.com>
2020-11-18 20:59:11 +0900

    [CC] feat: create connect mode sample.

--- src/careful-chameleon/sketches/ble-broadcast/src/main.cpp
+++ src/careful-chameleon/sketches/ble-broadcast/src/main.cpp
@@ -1,11 +1,36 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
 #include <M5Stack.h>
 
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define DEVICE_NAME "calyptratus"
+
 void setup() {
-    M5.begin();
-    M5.Power.begin();
-    M5.Lcd.print("Hello World");
+  M5.begin();
+  M5.Power.begin();
+  M5.Lcd.println("M5Stack launched.");
+
+  M5.Lcd.println("Starting BLE work!");
+  BLEDevice::init(DEVICE_NAME);
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic *pCharasteristic = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+
+  pCharasteristic->setValue("Hello World! This is test characteristic.");
+  pService->start();
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(
+      0x06); // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  M5.Lcd.println("Advertising...");
 }
 
-void loop() {
-
-}
+void loop() {}
