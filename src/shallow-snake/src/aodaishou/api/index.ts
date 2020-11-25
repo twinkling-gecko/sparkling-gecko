c3d2c7306eaa6db6024dd9312897196ac49a9f4d
tatikaze <k017c1356@it-neec.jp>
2020-11-25 13:28:53 +0900

    [SS] feat: add passport auth api

--- src/shallow-snake/src/aodaishou/api/index.ts
+++ src/shallow-snake/src/aodaishou/api/index.ts
@@ -1,6 +1,9 @@
 import 'reflect-metadata'
 
 import express from 'express'
+import passport from 'passport'
+import { Strategy as LocalStrategy } from 'passport-local'
+import { User } from './entity/User'
 import swaggerUi from 'swagger-ui-express'
 import swaggerJSDoc from 'swagger-jsdoc'
 import swaggerConfig from './swagger.config'
@@ -18,6 +21,35 @@ try {
 
 const app = express()
 
+app.use(passport.initialize())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: 'email', session: false },
+    async (email, password, done) => {
+      const user = await User.findOne({ email: email })
+
+      // FIXME: パスワードの暗号化処理
+      if (user?.encrypted_password === password) {
+        return done(null, { id: user.id, email: user.email })
+      } else {
+        return done(null, false, { message: 'invalid user' })
+      }
+    }
+  )
+)
+
+passport.serializeUser(function (user: User, done) {
+  done(null, user.id)
+})
+
+passport.deserializeUser(async function (id: number, done) {
+  const user = await User.findOne(id)
+  done(null, user)
+})
+
 // FIXME: アプリケーションが持つべきものではないのでgithub pagesとかに静的ファイルとしてアップロードすべき
 if (process.env.node_env != 'production') {
   app.use(
@@ -28,5 +60,9 @@ if (process.env.node_env != 'production') {
 }
 
 app.use(router)
+
+app.post('/session', passport.authenticate('local'), (_, res) => {
+  res.json({ message: 'success' })
+})
 
 export default app
