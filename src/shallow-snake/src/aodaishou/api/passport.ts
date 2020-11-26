95b001530cf7b000a3461d869fb80cb2b9c77b88
Sota Suzuki <k017c1356@it-neec.jp>
2020-11-26 18:07:45 +0900

    [SS] feat: handle passport deserializeUser error

--- src/shallow-snake/src/aodaishou/api/passport.ts
+++ src/shallow-snake/src/aodaishou/api/passport.ts
@@ -34,8 +34,13 @@ passport.serializeUser(function (user: User, done) {
 })
 
 passport.deserializeUser(async function (id: number, done) {
-  const user = await User.findOne(id)
-  done(null, user)
+  try {
+    const user: User | undefined = await User.findOne({ id: id })
+    console.log(user)
+    done(null, user)
+  } catch (err) {
+    done(err, false)
+  }
 })
 
 const comparePlainWithHash = async (plainPassword: string, hash: string) => {
