b550bb88be5fca916f49bd6ea398ed43f9c6a406
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2020-11-30 15:43:11 +0900

    [SS] feat: apply bootstrap component

--- src/shallow-snake/src/aodaishou/pages/index.vue
+++ src/shallow-snake/src/aodaishou/pages/index.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
+  <div
+    id="index"
+    class="d-flex align-items-center justify-content-center text-center"
+    :style="{height: `${height}px`}"
+  >
     <div>
       <Logo />
       <h1>sparkling-gecko</h1>
       <div>
-        <a href="https://nuxtjs.org/" target="_blank" rel="noopener noreferrer">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <b-button size="lg" variant="success">Sign up</b-button>&nbsp;
+        <b-button size="lg" variant="primary">Login</b-button>
       </div>
     </div>
   </div>
@@ -14,5 +18,33 @@
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  data() {
+    return {
+      height: 0,
+    }
+  },
+  created() {
+    if (process.client) {
+      window.addEventListener('resize', this.handleResize)
+      this.handleResize()
+    }
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.handleResize)
+  },
+  methods: {
+    handleResize() {
+      this.height = window.innerHeight
+    },
+  },
+})
 </script>
+
+<style lang="scss" scoped>
+#index {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
+
