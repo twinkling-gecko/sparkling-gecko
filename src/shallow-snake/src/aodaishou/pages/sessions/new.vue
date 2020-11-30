d551ec84ef25b708ddc6cda565f12fc7e923e536
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2020-11-30 16:36:41 +0900

    [SS] feat: create sessions/new page

---
+++ src/shallow-snake/src/aodaishou/pages/sessions/new.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="sessions-new">
+    <div class="text-center">
+      <h1>welcome back, dear wild gecko.</h1>
+    </div>
+    <div class="container" style="max-width: 720px;">
+      <b-form @submit="onSubmit" @reset="onReset">
+        <b-form-group label="email" label-for="email">
+          <b-form-input
+            id="email"
+            v-model="form.email"
+            type="email"
+            required
+            placeholder="Enter email"
+          />
+        </b-form-group>
+        <b-form-group label="password" label-for="password">
+          <b-form-input
+            id="password"
+            v-model="form.password"
+            type="password"
+            required
+            placeholder="Enter password"
+          />
+          <div class="text-right">
+            <b-link>Forgot password?</b-link>
+          </div>
+        </b-form-group>
+        <div class="text-center">
+          <b-button type="submit" variant="primary" block>Login</b-button>
+        </div>
+      </b-form>
+      <div class="text-center my-2">
+        <p>
+          New to Sparkling-Gecko?
+          <b-link>Create account.</b-link>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+      },
+    }
+  },
+})
+</script>
