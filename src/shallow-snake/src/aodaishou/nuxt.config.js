b6251d321c74a2c856f1485d5b3bfd3bcc371eeb
Sota Suzuki <k017c1356@it-neec.jp>
2020-12-05 23:38:19 +0900

    [SS] feat: vuex store persistence

--- src/shallow-snake/src/aodaishou/nuxt.config.js
+++ src/shallow-snake/src/aodaishou/nuxt.config.js
@@ -16,7 +16,7 @@ export default {
   css: ['@/assets/scss/common.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: [{ src: '@/plugins/nuxt-client-init.ts', ssr: false }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
