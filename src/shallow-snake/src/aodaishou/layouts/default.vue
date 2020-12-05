29c755bca64e17b756e0d782505a3269ad5e67db
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2020-12-05 22:23:16 +0900

    feat: add layout component name

--- src/shallow-snake/src/aodaishou/layouts/default.vue
+++ src/shallow-snake/src/aodaishou/layouts/default.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
     <b-navbar type="dark" variant="primary" class="mb-4">
-      <b-navbar-brand to="/">
-        sparkling-gecko
-      </b-navbar-brand>
+      <b-navbar-brand to="/"> sparkling-gecko </b-navbar-brand>
     </b-navbar>
     <Nuxt />
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Default',
+})
+</script>
--- src/shallow-snake/src/aodaishou/layouts/no_header.vue
+++ src/shallow-snake/src/aodaishou/layouts/no_header.vue
@@ -3,3 +3,11 @@
     <Nuxt />
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'NoHeader',
+})
+</script>
