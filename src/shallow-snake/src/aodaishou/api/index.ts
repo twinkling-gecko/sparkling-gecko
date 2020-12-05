3ab9244204274d34455e7473a5ad4b14bfb1061a
Sota Suzuki <k017c1356@it-neec.jp>
2020-12-06 02:09:08 +0900

    [SS] feat: remove swagger-jsdoc swagger-ui-express

--- src/shallow-snake/src/aodaishou/api/index.ts
+++ src/shallow-snake/src/aodaishou/api/index.ts
@@ -4,9 +4,6 @@ import express from 'express'
 import session from 'express-session'
 import cors from 'cors'
 import passport from 'passport'
-import swaggerUi from 'swagger-ui-express'
-import swaggerJSDoc from 'swagger-jsdoc'
-import swaggerConfig from './swagger.config'
 import { createConnection, getConnection } from 'typeorm'
 
 import router from './routes'
@@ -33,15 +30,6 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-// FIXME: アプリケーションが持つべきものではないのでgithub pagesとかに静的ファイルとしてアップロードすべき
-if (process.env.node_env != 'production') {
-  app.use(
-    '/docs',
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerJSDoc(swaggerConfig))
-  )
-}
 
 app.use(router)
 
