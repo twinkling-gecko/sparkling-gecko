80914f598f99d10953de87337e15fa017c0bf0b6
Ryota Kobayashi <38117745+kobar9568@users.noreply.github.com>
2020-12-14 15:50:22 +0900

    [CC] feat: add scale basic feature.

--- src/careful-chameleon/sketches/ble-broadcast/src/main.cpp
+++ src/careful-chameleon/sketches/ble-broadcast/src/main.cpp
@@ -1,14 +1,38 @@
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <M5Stack.h>
+#include <HX711.h>
 
 #define DEVICE_NAME "calyptratus"
 
 const int SLEEPING_TIME = 3;                  // Deep Sleep time length (sec)
 const int ADVERTISING_TIME = 3;               // Advertising time length (sec)
 
+// Load cell configs
+const int LOADCELL_DOUT_PIN = 2;
+const int LOADCELL_SCK_PIN = 3;
+const long DIV = 476;                         // TODO Calibration
+
+HX711 scale;
+RTC_DATA_ATTR long offset = 0;                // in Slow Memory
+
+void setOffset() {
+  M5.Lcd.println("Zero point calibrating...");
+  if (scale.is_ready()) {
+    offset = scale.read();
+  }
+}
+
+uint16_t measureWeight() {
+  if (scale.is_ready()) {
+    long reading = (scale.read() - offset) / DIV;
+    M5.Lcd.println("Measuring... data: " + String(reading));
+    return (uint16_t)reading;
+  }
+}
+
 void setAdvertisementData(BLEAdvertising *pAdvertising) {
-  uint16_t data = (uint16_t)2047;             // User Payload (2byte)
+  uint16_t data = measureWeight();            // User Payload (2byte)
   std::string strData = "";
   strData += (char)0xff;                      // AD Type (Manufacturer Specific Data)
   strData += (char)((data >> 8) & 0xff);      // User Payload High byte
@@ -25,6 +49,11 @@ void setup() {
   M5.begin();
   M5.Power.begin();
   M5.Lcd.println("M5Stack launched.");
+
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  if (offset == 0) {
+    setOffset();
+  }
 
   BLEDevice::init(DEVICE_NAME);
   BLEServer *pServer = BLEDevice::createServer();
