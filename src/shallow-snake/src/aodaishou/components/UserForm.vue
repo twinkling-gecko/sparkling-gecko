fe82dc34a7c800849e19110938a5089144ab1300
tatikaze <k017c1356@it-neec.jp>
2020-12-21 13:39:27 +0900

    [SS] feat: cut out UserForm

---
+++ src/shallow-snake/src/aodaishou/components/UserForm.vue
@@ -0,0 +1,67 @@
+<template>
+  <b-form @submit="onSubmit" @reset="onReset">
+    <b-alert :show="error !== ''" variant="danger">
+      {{ error }}
+    </b-alert>
+    <b-form-group label="email" label-for="email">
+      <b-form-input
+        id="email"
+        v-model="form.email"
+        type="email"
+        required
+        placeholder="Enter email"
+      />
+    </b-form-group>
+    <b-form-group label="password" label-for="password">
+      <b-form-input
+        id="password"
+        v-model="form.password"
+        type="password"
+        required
+        placeholder="Enter password"
+      />
+      <slot />
+    </b-form-group>
+    <div class="text-center">
+      <b-button type="submit" variant="primary" block>{{
+        submitText
+      }}</b-button>
+    </div>
+  </b-form>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    submitText: {
+      type: String,
+      default: '',
+    },
+    form: {
+      type: Object,
+      default: () => ({
+        email: '',
+        password: '',
+      }),
+    },
+    error: {
+      type: String,
+      default: '',
+    },
+    onSubmit: {
+      type: Function,
+      default: () => {
+        return false
+      },
+    },
+    onReset: {
+      type: Function,
+      default: () => {
+        return
+      },
+    },
+  },
+})
+</script>
--- src/shallow-snake/src/aodaishou/pages/sessions/new.vue
+++ src/shallow-snake/src/aodaishou/pages/sessions/new.vue
@@ -5,35 +5,17 @@
       <Logo />
     </div>
     <div class="container my-2" style="max-width: 720px">
-      <b-form @submit="onSubmit" @reset="onReset">
-        <b-alert :show="error !== ''" variant="danger">
-          {{ error }}
-        </b-alert>
-        <b-form-group label="email" label-for="email">
-          <b-form-input
-            id="email"
-            v-model="form.email"
-            type="email"
-            required
-            placeholder="Enter email"
-          />
-        </b-form-group>
-        <b-form-group label="password" label-for="password">
-          <b-form-input
-            id="password"
-            v-model="form.password"
-            type="password"
-            required
-            placeholder="Enter password"
-          />
-          <div class="text-right">
-            <b-link>Forgot password?</b-link>
-          </div>
-        </b-form-group>
-        <div class="text-center">
-          <b-button type="submit" variant="primary" block>Login</b-button>
+      <UserForm
+        :form="form"
+        :submitText="Login"
+        :error="error"
+        :onSubmit="onSubmit"
+        :onReset="onReset"
+      >
+        <div class="text-right">
+          <b-link>Forgot password?</b-link>
         </div>
-      </b-form>
+      </UserForm>
       <div class="text-center my-2">
         <p>
           New to Sparkling-Gecko?
