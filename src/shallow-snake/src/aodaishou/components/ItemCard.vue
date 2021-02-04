6d6cd819494796cb554c10fea877fe980d3a493f
Sota Suzuki <k017c1356@it-neec.jp>
2021-02-05 03:24:38 +0900

    [SS] feat: add item list page

---
+++ src/shallow-snake/src/aodaishou/components/ItemCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <b-card :title="item.name" :img-src="item.imageUrl" img-alt="Item image" img-top>
+      <b-button href="/items" variant="primary">view</b-button>
+    </b-card>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    item: {
+      type: Object,
+      default: () => ({
+        name: '',
+        imageUrl: '',
+      }),
+    },
+  },
+})
+</script>
