3f40fddf2efb193ee7e20a7b1fa872100e6f7a42
Sota Suzuki <k017c1356@it-neec.jp>
2021-02-05 03:24:38 +0900

    [SS] feat: add item list page

---
+++ src/shallow-snake/src/aodaishou/components/ItemList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="m-2">
+    <div v-for="item in items" class="card-container mx-2">
+      <ItemCard :item="item" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    items: Array,
+    default: () => [],
+  },
+})
+</script>
+
+<style lang="sass" scoped>
+.card-container
+  max-width: 360px
+  float: left
+</style>
---
+++ src/shallow-snake/src/aodaishou/pages/items/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-button href="/items/new" variant="primary">Add item</b-button>
+    <ItemList :items="items" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  async asyncData({ $axios }) {
+    const items = await $axios.$get('/api/v1/items')
+    return { items }
+  },
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          name: 'item1',
+          imageUrl:
+            'https://cdn.wikiwiki.jp/to/w/nijisanji/%E7%AB%9C%E8%83%86%E5%B0%8A/::ref/face.png?rev=a2c9e3f1de8f75c04278a071d2c9263c&t=20190717144820',
+        },
+        {
+          id: 2,
+          name: 'item2',
+          imageUrl:
+            'https://cdn.wikiwiki.jp/to/w/nijisanji/%E7%AB%9C%E8%83%86%E5%B0%8A/::ref/face.png?rev=a2c9e3f1de8f75c04278a071d2c9263c&t=20190717144820',
+        },
+      ],
+    }
+  },
+})
+</script>
