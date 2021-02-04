846b730b3a8f31f00ed53835e3fa0ce178f725f4
Sota Suzuki <k017c1356@it-neec.jp>
2021-02-05 03:23:18 +0900

    [SS] feat: add item creation and read api

--- src/shallow-snake/src/aodaishou/api/routes/v1/index.ts
+++ src/shallow-snake/src/aodaishou/api/routes/v1/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import sessionsRouter from './sessions'
 import usersRouter from './users'
+import itemsRouter from './items'
 
 const router = Router()
 
 router.use('/sessions', sessionsRouter)
 router.use('/users', usersRouter)
+router.use('/items', itemsRouter)
 
 export default router
---
+++ src/shallow-snake/src/aodaishou/api/routes/v1/items/index.ts
@@ -0,0 +1,58 @@
+import { Router, Request } from 'express'
+import { ItemBody } from '../../../validators'
+import '../../../passport'
+import { validate } from 'class-validator'
+import isAuthenticated from '../../../middleware/isAuthenticated'
+import { Item, User } from '../../../entity'
+
+const router = Router()
+
+/**
+ * @swagger
+ * /api/v1/sessions/new:
+ *   post:
+ *     tags: [v1]
+ *     description: item creation
+ *     responses:
+ *       200:
+ *         examples:
+ *           result:
+ *             message: string
+ */
+router.post('/new', isAuthenticated, async (req: Request, res) => {
+  console.log(req.user)
+  const user = req.user as User
+  let itemBody = new ItemBody()
+  itemBody.name = req.body.name
+  itemBody.image_url = req.body.image_url
+
+  let errors = await validate(itemBody)
+  if (errors.length > 0) return res.status(400).json(errors)
+
+  // TODO: fix name
+  let newItem = new Item()
+  newItem.name = itemBody.name
+  newItem.imageUrl = itemBody.image_url
+  newItem.user = user
+
+  errors = await validate(newItem)
+  if (errors.length > 0) return res.status(400).json(errors)
+
+  try {
+    newItem.save()
+    return res.status(200).json({ message: 'success' })
+  } catch (errors) {
+    return res.status(400).json(errors)
+  }
+})
+
+router.get('/', isAuthenticated, async (req, res) => {
+  try {
+    const users = await Item.find({ user: req.user })
+    res.status(200).json(users)
+  } catch (errors) {
+    res.status(400).json(errors)
+  }
+})
+
+export default router
