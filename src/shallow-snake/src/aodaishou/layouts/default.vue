cd5d1986da03de5bc5b5e7a39f70e7bd7c2669a5
tatikaze <k017c1356@it-neec.jp>
2021-02-09 22:28:14 +0900

    [SS] feat: add setting menu icon

--- src/shallow-snake/src/aodaishou/layouts/default.vue
+++ src/shallow-snake/src/aodaishou/layouts/default.vue
@@ -2,6 +2,20 @@
   <div>
     <b-navbar type="dark" variant="primary" class="mb-4">
       <b-navbar-brand to="/"> sparkling-gecko </b-navbar-brand>
+      <b-navbar-toggle target="navbar-toggle-collapse">
+        <template #default="{ expanded }">
+          <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+          <b-icon v-else icon="chevron-bar-down"></b-icon>
+        </template>
+      </b-navbar-toggle>
+
+      <b-collapse id="navbar-toggle-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">
+            <b-icon icon="gear-fill" />
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
     <Nuxt />
   </div>
